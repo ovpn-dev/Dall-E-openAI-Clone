c862c485fd9d16cf697ec70506f4b5f100a504dd
ovpn-dev <ovpn05@gmail.com>
2023-01-27 08:01:44 -0600

    initial commit

---
+++ openai-clone/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Configuration, OpenAIApi} from 'openai';
+import './App.css';
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("")
+  const configuration = new Configuration({
+  apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024"
+    });
+
+    setResult(res.data.data[0].url);
+  }
+  return (
+    <div className="app-main">
+      <h3>Generate an image using OpenAI API</h3>
+      <input
+        className="app-input"
+        placeholder="Type something to generate an image."
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+        <button onClick={generateImage}>Generate an image</button>
+
+        {result.length > 0 ? <img className="result-image" src={result} alt="result" /> : <></>}
+  </div>
+  );
+}
+
+export default App
